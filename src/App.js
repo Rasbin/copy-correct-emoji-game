e4221fee558fe169dda9daeb60c5658d2fff4a07
Rasbin Rijal <rasbin.rijal@gmail.com>
2024-05-15 21:30:41 +0300

    implement copy correct emoji game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const emojis = [
+  {
+    id: "frown",
+    emoji: "😞",
+  },
+  {
+    id: "mid",
+    emoji: "😐",
+  },
+  {
+    id: "smile",
+    emoji: "🙂",
+  },
+];
+
+const selectRandomElement = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
 
 function App() {
+  const selected = selectRandomElement(emojis);
+
+  const handleCopy = (e) => {
+    if (e.target.innerText !== selected.emoji) {
+      alert("Wrong emoji!");
+    } else if (e.timeStamp > 5000) {
+      alert("Too slow. Reset and try again.");
+    } else {
+      alert("🎉 Winner!");
+    }
+  };
+
+  const handleReset = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{selected.emoji}</h1>
+      <p>Copy this emoji</p>
+      <ul>
+        {emojis.map(({ id, emoji }) => {
+          return (
+            <li onCopy={handleCopy} key={id}>
+              {emoji}
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
